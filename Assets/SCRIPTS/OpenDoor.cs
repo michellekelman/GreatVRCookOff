4b5a6fb2b0bf98399683f500ac4f14dbbe64f5c6
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-04-05 10:22:25 -0500

    Inventory Setup with control functions

--- Assets/SCRIPTS/OpenDoor.cs
+++ Assets/SCRIPTS/OpenDoor.cs
@@ -8,21 +8,25 @@ public class OpenDoor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public GameObject objectName;
     bool hover;
     bool open;
+    string keyX;
     // Start is called before the first frame update
     void Start()
     {
         hover = false;
         open = false;
+
+    // <- Key Mappings -> 
+        keyX="js0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("js0") && hover==true && open==false) {
+        if(Input.GetButtonDown(keyX) && hover==true && open==false) {
             objectName.transform.eulerAngles = new Vector3(0.0f, 60.0f, 0.0f);
             open = true;
         }
-        else if(Input.GetButtonDown("js0") && hover==true && open==true) {
+        else if(Input.GetButtonDown(keyX) && hover==true && open==true) {
             objectName.transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
             open = false;
         }
