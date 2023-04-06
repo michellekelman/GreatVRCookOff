62ac362673f09c151d6ef98170e39494324a86af
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-04-06 17:40:04 -0500

    Temp, updated player menu,Active Object controller

---
+++ Assets/SCRIPTS/ActiveGameObject.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveGameObject : MonoBehaviour
+{
+    GameObject activeObject; 
+    GameObject inHandObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject getActiveObject() {
+        return activeObject;
+    }
+
+    public void setActiveObject(GameObject obj) {
+        activeObject = obj;
+        // Debug.Log(activeObject);
+    }
+
+    public void setInHandObject(GameObject obj) {
+        inHandObject = obj;
+    }
+}
---
+++ Assets/SCRIPTS/TempMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMenu : MonoBehaviour
+{
+    public GameObject pointerDial;
+    public GameObject player;
+    public GameObject tempMenu;
+    GameObject stove;
+    bool inputProcessed;
+    int pointerAngle; 
+    bool tempOn;
+    string X;
+    // Start is called before the first frame update
+    void Start()
+    {
+        X = "js1";
+        inputProcessed = false;
+        pointerAngle = 45;
+        tempOn = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stove = player.GetComponent<ActiveGameObject>().getActiveObject();
+        if(Input.GetButtonDown(X)) {
+            Debug.Log("X is pressed!");
+        }
+        //pressing X to interact with stove
+        if(Input.GetButtonDown(X) && 
+            player.GetComponent<ActiveGameObject>().getActiveObject() != null &&
+            player.GetComponent<ActiveGameObject>().getActiveObject().name.Contains("Stove")) 
+        {
+            tempOn = !tempOn; 
+            tempMenu.SetActive(tempOn); // toggling the dial menu 
+            player.transform.GetComponent<CharacterMovement>().enabled = !tempOn; // toggling character movement 
+            Debug.Log("Show Stove Dial" + tempOn);
+
+            if(!tempOn) // if false => closing menu
+            {
+                // get temp reading here.
+            }
+        }
+
+
+        // Dial Rotation
+        float inputH = -1*Input.GetAxisRaw("Horizontal");
+        float inputV = -1*Input.GetAxisRaw("Vertical");
+
+        if(tempOn) {
+            if(inputV>0 && !inputProcessed){
+                //TODO: Rotate dial and reset 
+                rotateDialAntiClockwise();
+                inputProcessed = true;
+            }
+            if(inputV<0 && !inputProcessed){
+                rotateDialClockwise();
+                inputProcessed = true;
+            }
+            if(inputV == 0){
+                inputProcessed = false;
+            }
+        }
+    }
+
+    void rotateDialAntiClockwise() {
+        pointerDial.transform.Rotate(0, 0, pointerAngle);
+    }
+
+    void rotateDialClockwise() {
+        pointerDial.transform.Rotate(0, 0, -pointerAngle);
+    }
+
+
+}
