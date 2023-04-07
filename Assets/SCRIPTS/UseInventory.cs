2263c3d4c4a4b5df77514963bffbb5e24383ac9f
Chethan B N <47515084+Chethan30@users.noreply.github.com>
2023-04-07 09:53:58 -0500

    Inventory - adding items, teleporting to table.

--- Assets/SCRIPTS/UseInventory.cs
+++ Assets/SCRIPTS/UseInventory.cs
@@ -20,15 +20,34 @@ public class UseInventory : MonoBehaviour
         if(Input.GetButtonDown(X) && activeObj != null) 
         { 
             Debug.Log("Here" + activeObj.name);
-            if( activeObj.name.Contains("Bread") ||
-              activeObj.name.Contains("Cheese") ||
-                activeObj.name.Contains("Butter")) {
-                // GameObject activeObj = player.GetComponent<ActiveGameObject>().getActiveObject();
-                player.GetComponent<InventoryMenu>().addObj(activeObj.name, activeObj);
+            if( activeObj.name.Contains("Bread")) {
+                player.GetComponent<InventoryMenu>().addObj("Bread", activeObj); 
+                activeObj.SetActive(false); 
+            }
+            else if(activeObj.name.Contains("Cheese")) {
+                player.GetComponent<InventoryMenu>().addObj("Cheese", activeObj); 
+                activeObj.SetActive(false);
+            } 
+            else if (activeObj.name.Contains("Butter")) {
+                player.GetComponent<InventoryMenu>().addObj("Butter", activeObj); 
                 activeObj.SetActive(false);
             }
+            player.GetComponent<InventoryMenu>().loadInventory();
         }
+
+        if(Input.GetButtonDown(X) && activeObj != null) {
+            int itemsInInvent = player.GetComponent<InventoryMenu>().getCount();
+            if( activeObj.name.Contains("Table") && itemsInInvent==3) {
+                int startIndex = activeObj.name.IndexOf("(");
+                string boardNumber = activeObj.name.Substring(startIndex+1,1);
+                GameObject kitchenItems = GameObject.Find("KitchenItems "+"("+boardNumber+")");
+                GameObject foodBoard = kitchenItems.transform.GetChild(0).gameObject;
+                foodBoard.SetActive(true);
+                player.GetComponent<InventoryMenu>().hideInvent();
+                activeObj.GetComponent<Outline>().enabled = false;
+                activeObj.GetComponent<Highlight>().enabled = false;
+            }
+        }
+        
     }
-
-
 }
